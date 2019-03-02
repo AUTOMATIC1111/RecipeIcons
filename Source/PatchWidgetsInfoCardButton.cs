722a7794c4ea7c6369ebe0b8f189239674a699d1
AUTOMATIC1111 <16777216c@gmail.com>
2019-03-02 10:02:10 +0300

    add support for operation icons

--- Source/PatchWidgetsInfoCardButton.cs
+++ Source/PatchWidgetsInfoCardButton.cs
@@ -13,31 +13,64 @@ namespace RecipeIcons
     [HarmonyPatch(typeof(Widgets), "InfoCardButton", new Type[] { typeof(float), typeof(float), typeof(Def) }), StaticConstructorOnStartup]
     class PatchWidgetsInfoCardButton
     {
-        static bool Prefix(float x, float y, Def def)
+        class Icon {
+            public Texture2D icon = null;
+            public Color color = Color.black;
+            
+            public Icon(ThingDef thing) {
+                icon = thing.uiIcon;
+                color = thing.uiIconColor;
+            }
+        }
+
+        static Dictionary<Def, Icon> map = new Dictionary<Def, Icon>();
+        static Icon getIcon(Def def)
         {
-            Texture2D icon = null;
-            Color color = GUI.color;
+            Icon res;
 
+            if (map.TryGetValue(def, out res)) return res;
+
+            res = getIconWithoutCache(def);
+            if (res == null) return null;
+
+            map.Add(def, res);
+            return res;
+        }
+
+        static Icon getIconWithoutCache(Def def) {
             RecipeDef recipe = def as RecipeDef;
-            if (recipe != null) {
-                if (recipe.products.Count == 0) return true;
-                if (recipe.products[0].thingDef == null) return true;
+            if (recipe != null)
+            {
+                if (recipe.products.Count != 0)
+                {
+                    if (recipe.products[0].thingDef == null) return null;
 
-                icon = recipe.products[0].thingDef.uiIcon;
-                color = recipe.products[0].thingDef.uiIconColor;
+                    return new Icon(recipe.products[0].thingDef);
+                }
+
+                foreach(IngredientCount ing in recipe.ingredients) {
+                    if (ing == null) continue;
+                    if (!ing.IsFixedIngredient) continue;
+                    return new Icon(ing.FixedIngredient);
+                }
             }
 
             ThingDef thing = def as ThingDef;
             if (thing != null)
             {
-                icon = thing.uiIcon;
-                color = thing.uiIconColor;
+                return new Icon(thing);
             }
 
+            return null;
+        }
+
+        static bool Prefix(float x, float y, Def def)
+        {
+            Icon icon = getIcon(def);
             if (icon == null) return true;
 
             Rect rect = new Rect(x, y, 24f, 24f);
-            if (Widgets.ButtonImage(rect, icon, color))
+            if (Widgets.ButtonImage(rect, icon.icon, icon.color))
             {
                 Find.WindowStack.Add(new Dialog_InfoCard(def));
                 return true;
